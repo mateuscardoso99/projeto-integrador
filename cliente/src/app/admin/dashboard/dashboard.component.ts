230b8acb3696f293eed1402f8e69e82ee7f91045
mat475000 <mat358591@gmail.com>
2024-05-26 10:29:59 -0300

    .

--- cliente/src/app/admin/dashboard/dashboard.component.ts
+++ cliente/src/app/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- cliente/src/app/app.routes.ts
+++ cliente/src/app/app.routes.ts
@@ -6,12 +6,15 @@ import { CategoriaComponent } from './admin/categoria/categoria.component';
 import { QuestaoComponent } from './admin/questao/questao.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { JogarComponent } from './jogar/jogar.component';
+import { HomeComponent } from './admin/home/home.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent },
     { path: 'criar-conta', component: CriarContaComponent },
     { path: 'admin', component: DashboardComponent,
         children: [
+            { path: '', component: HomeComponent, pathMatch: 'full' },
             { path: 'categorias', component: CategoriaComponent },
             { path: 'questoes', component: QuestaoComponent },
         ]
